530144300a91af668607963117c7130a30bc6c9a
mayank7463 <mayank18091999@gmail.com>
2024-07-29 12:19:14 +0530

    add mc

--- src/Pages/Capitals/Manufacturecapital/ManufactureCapitals.jsx
+++ src/Pages/Capitals/Manufacturecapital/ManufactureCapitals.jsx
@@ -39,23 +39,112 @@ fulfil the promise of aesthetics, functionality and durability, we continue to e
         <img className='rounded-lg w-[80%] m-auto ' src="./Stakeholder/mc.png" alt="" />
         </div>
     </div>
-    <div className='w-[86%] m-auto my-5 n-t-p'>
-        <b className='text-[#1f5dad]'>Our manufacturing facilities</b>
-      <p className='n-t-p'>We have advanced manufacturing facilities in Chakan, Nashik and Aurangabad. Reiterating our commitment to quality and excellence, we have developed state-of-the-art manufacturing infrastructure to produce innovative and premium products.
-With a consistent focus on improving operational efficiency, we continue
-to upgrade the existing machinery
-and adopt technologically advanced processes. It not only helps us to adhere to rigorous quality parameters but also ensures optimum resource utilisation.
-</p>
-     <div className='flex justify-between mt-8'>
-  <div className='md:w-[30%]'>
-    <img className='md:w-[80%] ' src="./Stakeholder/map.png" alt="" />
-    <img src="./Stakeholder/bajaj005.png" alt="" />
-  </div>
-  <div className='md:w-[70%]'>
-    <img className=' w-full md:w-[90%]  object-contain' src="./Stakeholder/bajaj0012.png" alt="" />
-  </div>
-     </div>
-     
+    <div className='w-[90%] m-auto'>
+        <div className='w-[86%] m-auto my-5 n-t-p'>
+            <b className='text-[#1f5dad]'>Our manufacturing facilities</b>
+            <p className='n-t-p'>We have advanced manufacturing facilities in Chakan, Nashik and Aurangabad. Reiterating our commitment to quality and excellence, we have developed state-of-the-art manufacturing infrastructure to produce innovative and premium products.
+    With a consistent focus on improving operational efficiency, we continue
+    to upgrade the existing machinery
+    and adopt technologically advanced processes. It not only helps us to adhere to rigorous quality parameters but also ensures optimum resource utilisation.
+    </p>
+        <div className='flex justify-between mt-8'>
+      <div className='md:w-[30%]'>
+        <img className='md:w-[80%] ' src="./Stakeholder/map.png" alt="" />
+        <img src="./Stakeholder/bajaj005.png" alt="" />
+      </div>
+      <div className='md:w-[70%]'>
+        <img className=' w-full md:w-[90%]  object-contain' src="./Stakeholder/bajaj0012.png" alt="" />
+      </div>
+        </div>
+        
+        </div>
+        <div className='flex lg:flex-row flex-col w-[90%] m-auto pt-16'>
+              <div className=''>
+                  <img src="./MC/manc-1.png" alt="" />
+              </div>
+              <div>
+                  <div>
+                      <img src="./MC/manc-2.png" alt="" />
+                  </div>
+                  <div>
+                      <img src="./MC/manc-3.png" alt="" />
+                  </div>
+              </div>
+        </div>
+        <div className='flex lg:flex-row flex-col w-[90%] m-auto pt-16'>
+             <div>
+                 <img src="./MC/manc-4.png" alt="" />
+             </div>
+             <div >
+                  <div className='flex lg:flex-row flex-col'>
+                      <div>
+                           <img src="./MC/manc-6.png" alt="" />
+                      </div>
+                      <div>
+                           <img src="./MC/manc-7.png" alt="" />
+                      </div>
+                  </div>
+                  <div className=''>
+                       <img src="./MC/manc-8.png" alt="" />
+                  </div>
+             </div>
+        </div>
+        <div className='w-[90%] m-auto my-8'>
+            <div>
+                <img src="./MC/manc-9.png" alt="" />
+            </div>
+            <div className='flex lg:flex-row flex-col'>
+                <div>
+                     <img src="./MC/manc-10.png" alt="" />
+                </div>
+                <div>
+                     <img src="./MC/manc-11.png" alt="" />
+                </div>
+                <div>
+                      <img src="./MC/manc-12.png" alt="" />
+                </div>
+            </div>
+        </div>
+        <div className='w-[90%] m-auto flex lg:flex-row flex-col my-8'>
+             <div>
+                  <img src="./MC/manc-13.png" alt="" />
+             </div>
+             <div>
+                 <div className='flex lg:flex-row flex-col'>
+                     <div>
+                         <img src="./MC/manc-14.png" alt="" />
+                     </div>
+                     <div>
+                          <img src="./MC/manc-15.png" alt="" />
+                     </div>
+                 </div>
+                 <div>
+                       <img src="./MC/manc-16.png" alt="" />
+                 </div>
+             </div>
+        </div>
+        <div className='w-[90%] m-auto flex lg:flex-row flex-col my-8'>
+            <div>
+                <img src="./MC/manc-17.png" alt="" />
+            </div>
+            <div>
+              <img src="./MC/manc-18.png" alt="" />
+            </div>
+            <div>
+                <img src="./MC/manc-19.png" alt="" />
+            </div>
+        </div>
+        <div className='w-[90%] m-auto flex lg:flex-row flex-col my-8'>
+            <div>
+                <img src="./MC/manc-20.png" alt="" />
+            </div>
+            <div>
+              <img src="./MC/manc-21.png" alt="" />
+            </div>
+            <div>
+                <img src="./MC/manc-22.png" alt="" />
+            </div>
+        </div>
     </div>
     </>
   )
